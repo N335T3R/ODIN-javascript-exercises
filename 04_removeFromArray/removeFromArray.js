d548ba147d608e8df54fd292dfb3fad019a1ae84
N335T3R <n335t3r@gmail.com>
2023-06-30 15:45:45 -0400

    All 04 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,13 +2,19 @@
 
 const removeFromArray = function(array, ...nums) {
     for (let num of nums) {
-        let waste = array.splice(num - 1, 1);
+        let isInArray = array.includes(num);
+
+        if (isInArray == false) {
+            continue;
+        } else if (isInArray == true) {
+            let waste = array.splice(array.indexOf(num), 1);
+        }
     }
     return array;
 } 
 
 
-removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
