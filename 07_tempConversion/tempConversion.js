d2010ac568f15d0361670840d17f175615930e92
N335T3R <n335t3r@gmail.com>
2023-06-30 17:57:11 -0400

    All 07 passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp -32) * 0.5556;
+  let round = temp.toFixed(1);
+  let result = parseFloat(round);
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 1.8) + 32;
+  let round = temp.toFixed(1);
+  let result = parseFloat(round);
+  return result;
 };
 
 // Do not edit below this line
