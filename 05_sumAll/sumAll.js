58c241a501a80460a662d01f2e13f3dc4e81ec94
N335T3R <n335t3r@gmail.com>
2023-06-30 16:10:45 -0400

    All 06 passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,18 @@
 const sumAll = function(num1, num2) {
     let sum = num1;
 
-    for (let i = num1 + 1; i <= num2; i++) {
-        sum = sum + i;
+    if (typeof num1 !== "number"  || typeof num2 !== "number") {
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num1 < num2) {
+        for (let i = num1 + 1; i <= num2; i++) {
+            sum = sum + i;
+        }
+    } else if (num1 > num2) {
+        for (let i = num1 - 1; i >= num2; i--) {
+            sum = sum + i;
+        }
     }
 
     return sum;
