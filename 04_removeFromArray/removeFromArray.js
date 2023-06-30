479a64009564e2c746bd2dd3ed000144ccc46a79
N335T3R <n335t3r@gmail.com>
2023-06-30 15:32:26 -0400

    Test 2 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
 
 
-const removeFromArray = function(array, num) {
-    let waste = array.splice(num - 1, 1);
-    return array; 
-}
+const removeFromArray = function(array, ...nums) {
+    for (let num of nums) {
+        let waste = array.splice(num - 1, 1);
+    }
+    return array;
+} 
 
 
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
