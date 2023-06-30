4f7377743060a9e29a00d769acdec348c6ccf844
N335T3R <n335t3r@gmail.com>
2023-06-30 15:57:16 -0400

    First 05 test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = num1;
 
+    for (let i = num1 + 1; i <= num2; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
