ad915d3a9c96672451465a0521cc48f273022997
N335T3R <n335t3r@gmail.com>
2023-06-30 17:38:27 -0400

    All 06 passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let exp = new RegExp (/[0-9]{1,9}/);
+    let test = exp.test(year);
 
-};
+    let cen = new RegExp (/0{2}$/)
+    let cenTest = cen.test(year)
+
+    if (test == true && cenTest == true) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (test == true && cenTest == false) {
+        if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (test == false && cenTest == false) {
+        return false;
+    }
+}
+
+leapYears();
 
 // Do not edit below this line
 module.exports = leapYears;
