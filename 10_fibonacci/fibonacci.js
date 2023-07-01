152de6ccb6210db9d70d1ad8d5a7da572549c1b3
N335T3R <n335t3r@gmail.com>
2023-06-30 21:26:40 -0400

    All 10 passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,56 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    } else if (typeof index !== "number") {
+        index = parseInt(index);
 
+        let num1 = 1;
+        let num2 = 1;
+
+        let fib = [1, 1];
+        let a = 2;
+        sum = num1 + num2;
+        fib[a] = sum;
+        a++;
+
+        for (let i = 0; i < 25; i++) {
+            num2 = sum;
+            sum = num1 + num2;
+            fib[a] = sum;
+            a++;
+
+            num1 = sum;
+            sum = num1 + num2;
+            fib[a] = sum;
+            a++;
+        }
+
+        return fib[index - 1];
+    } else {
+
+        let num1 = 1;
+        let num2 = 1;
+
+        let fib = [1, 1];
+        let a = 2;
+        sum = num1 + num2;
+        fib[a] = sum;
+        a++;
+
+        for (let i = 0; i < 25; i++) {
+            num2 = sum;
+            sum = num1 + num2;
+            fib[a] = sum;
+            a++;
+
+            num1 = sum;
+            sum = num1 + num2;
+            fib[a] = sum;
+            a++;
+        }
+
+        return fib[index - 1];
+    }
 };
 
 // Do not edit below this line
