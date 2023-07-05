8f9220e194efc4361ed2bc9c597160f1b18296ec
N335T3R <n335t3r@gmail.com>
2023-07-05 10:13:19 -0400

    1st 11 passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let titles = array.map((value) => {
+        for(let property in value) {
+            return(`${value[property]}`);
+        }
+    });
 
+    return titles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
